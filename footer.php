c48b9f3fa2e06b9a549196a595cc94b742db0e7c
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-03-19 19:29:06 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+</div>
+<!--footer-->
+  <footer>
+    <div class="container-fluid mt-0">
+    </div>
+    <div class="container-fluid text-muted pt-3" style="background-color: #000">
+      <div class="row">
+        <div class="col-12 col-lg-6">
+            <p>&copy; 2020 HailCar. All Rights Reserved </p>
+        </div>
+        <div class="col-12 col-lg-6">
+            <p class="float-lg-right">Designed by <span class="text-primary">Group 1</span></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
