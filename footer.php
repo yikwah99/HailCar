2c0d55335bb25b87bc3342d9ac9b6e848553e3cd
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-03-20 23:31:26 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
   <footer>
     <div class="container-fluid mt-0">
     </div>
-    <div class="container-fluid text-muted pt-3" style="background-color: #000">
+    <div class="container-fluid text-dark pt-3 bg-warning">
       <div class="row">
         <div class="col-12 col-lg-6">
             <p>&copy; 2020 HailCar. All Rights Reserved </p>
         </div>
         <div class="col-12 col-lg-6">
-            <p class="float-lg-right">Designed by <span class="text-primary">Group 1</span></p>
+            <p class="float-lg-right">Designed by <span class="text-info">Group 1</span></p>
         </div>
       </div>
     </div>
