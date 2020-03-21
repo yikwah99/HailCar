757b7e8f45db7fc86722cc3ec0b1febc6bf8f4a7
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-03-21 14:41:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
   include "header.php";
 ?>
 
-
+<div class="container-fluid">
+  <div class="carousel-item">
+  <img src="..." alt="...">
+  <div class="carousel-caption d-none d-md-block">
+    <h5>...</h5>
+    <p>...</p>
+  </div>
+</div>
+</div>
 
 
 
