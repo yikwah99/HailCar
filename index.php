185b319f7b6792074ee53e821aab5f64595ed65a
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-03-25 02:25:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,37 @@
 ?>
 <style>
   html{
+    scroll-behavior: smooth;
     overflow-x:hidden;
+  }
+  .accordion {
+    background-color: #eee;
+    color: #444;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    transition: 0.4s;
+  }
+
+  .panel {
+    padding: 0 18px;
+    display: none;
+    background-color: white;
+    overflow: hidden;
   }
 </style>
 
 <div class="jumbotron bg-white">
-  <h1 class="display-4 text-center">Welcome To</h1><h1 class="display-4 text-warning text-center mb-5 zoom">HailCar</h1>
+  <h1 class="display-4 text-center">Welcome To</h1>
+  <h1 class="display-4 text-warning text-center mb-5 zoom">HailCar</h1>
+    <p class="text-center">Services</p>
   <div class="card-group">
   <div class="card p-3">
-    <h1 class="text-center text-warning zoom">HAIL CAR</h1>
+    <h1 class="display-4 text-center text-warning">HAIL CAR</h1>
     <div class="card-body">
       <h5 class="card-title text-center">Book a ride to travel around the beautiful CITY.</h5>
 
@@ -20,7 +42,7 @@
     </div>
   </div>
   <div class="card p-3">
-    <h1 class=" text-center text-success zoom">HAIL SHARE</h1>
+    <h1 class="display-4 text-center text-success">HAIL SHARE</h1>
     <div class="card-body">
       <h5 class="card-title text-center">Share a ride to travel around the city with lower PRICES.</h5>
 
@@ -28,7 +50,7 @@
     </div>
   </div>
   <div class="card p-3">
-    <h1 class="text-center text-info zoom">HAIL FOOD</h1>
+    <h1 class="display-4 text-center text-info ">HAIL FOOD</h1>
     <div class="card-body">
       <h5 class="card-title">We can deliver delicious food to satisfy your CRAVINGS.</h5>
 
@@ -36,9 +58,9 @@
     </div>
   </div>
 </div>
+<div class="col-md-12 mt-5 mb-5 text-center">
+  <a class="btn btn-outline-info zoom" href="#aboutUs">Learn More</a>
 </div>
-<div class="col-md-12 mb-5 text-center">
-  <button class="btn btn-outline-warning animatedBtn">Learn More</button>
 </div>
 
 <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
@@ -49,21 +71,14 @@
   </ol>
   <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="d-block w-100 darken" src="img/taxis.jpg" alt="First slide">
+        <img class="d-block w-100 darken" src="img/taxi2.jpg" alt="First slide">
         <div class="carousel-caption d-none d-md-block">
         <h5>...</h5>
         <p>...</p>
       </div>
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100 darken" src="img/taxis.jpg" alt="Second slide">
-        <div class="carousel-caption d-none d-md-block">
-        <h5>...</h5>
-        <p>...</p>
-      </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 darken" src="img/taxis.jpg" alt="Third slide">
+        <img class="d-block w-100 darken" src="img/taxi3.jpg" alt="Third slide">
         <div class="carousel-caption d-none d-md-block">
         <h5>...</h5>
         <p>...</p>
@@ -79,23 +94,80 @@
       <span class="sr-only">Next</span>
     </a>
 </div>
-
-
+<p id="aboutUs"></p>
 <!-- About Us -->
 <div class="container-fluid bg-white">
-  <h1 class="text-center mt-5">About Us</h1>
-  <br>
-  <br>
-  <br>
-  <br>
+  <h1 class="text-center mt-5 mb-5">About Us</h1>
+  <div class="col-md-12 row">
+    <div class="col-md-6">
+      <h3 class="text-warning text-center">Aim</h3>
+      <p>The website was originally designed to support very minimal tasks, with even more minimal interaction between the user and itself. These tasks include:
+        <ul>
+        <li>Allowing users to log in to their own accounts.</li>
+        <li>Allowing users to check their own personal loyalty rewards</li>
+        <li>Allowing users to send inquiries in the form of an email via the complaint box in the ‘Contact Us’ page.</li>
+        <li>Navigate to either the Google Play Store or Apple App Store to download and install the official HAIL application into the user’s personal devices.</li>
+        <li>Navigate the user to each of HAIL’s official social media pages to find out more about HAIL’s current affairs.</li>
+        <li>Educate all users about the services HAIL provides for them and how to make the most of them.</li>
+        <ul>
+      </p>
+    </div>
+    <div class="col-md-6">
+      <h3 class="text-warning text-center">Purpose</h3>
+      <p>The HAIL website was designed to be an informative website that provides essential information regarding HAIL’s services towards its current and prospective users.
+         HAIL as a service provider is conceptualised similarly to that of Grab, Uber or Lyft.
+         It is an e-hailing service but the website itself does not include any e-hailing functions.<p><br>
+         The targeted users of the website are people who are interested to find out more about HAIL, or people who would like to make enquiries about the services HAIL provides.
+         For instance, to learn about HAIL’s services, payment methods or rewards system, there are corresponding pages for each respective topic and of course, designated contact details are provided on
+         all pages of the website.</p>
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- FAQ -->
+<div class="container-fluid bg-white" id="faq">
+  <h2 class="text-center mt-5">FAQ</h2>
+    <button class="accordion">Section 1</button>
+    <div class="panel">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <button class="accordion">Section 2</button>
+    <div class="panel">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <button class="accordion">Section 3</button>
+    <div class="panel">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+</div>
+
+<!-- Contact Us -->
+<div class="container-fluid bg-white" id="contactUs">
+  <h1 class="text-center mt-5 mb-5">Contact Us</h1>
 
 </div>
 
 
 
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-
-
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+</script>
 <?php
   include "footer.php";
 ?>
