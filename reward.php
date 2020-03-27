d03d0af5662a215d2117d455d9e7bd4a59a0f17a
DylanChng <47546918+DylanChng@users.noreply.github.com>
2020-03-28 00:52:03 +0800

    Update reward.php

--- reward.php
+++ reward.php
@@ -2,6 +2,27 @@
   $page_title = "Hail Rewards";
   include "header.php";
 ?>
+<div class="container">
+  <div class=" text-center bg-white">
+    <h1 class="display-4">HAIL Rewards</h1>
+    <p class="lead">Hey there! HAIL provides services that only get better!</p>
+    <div class="container"><img class="img-fluid" src="img/line2.png" alt="bigBG"></div>
+    <p class="lead">The more you enjoy, the more you get rewarded!</p>
+  </div>
+
+  <div class=" container text-center">
+      <h1 class="display-4">Ranking</h1>
+    <div class="p-2 mb-2 text-white zoom" style="background:#cd7f32;">Bronze 0-250 Points</div>
+    <div class="p-2 mb-2 text-dark zoom" style="background:#C0C0C0;">Silver 250-750 Points</div>
+    <div class="p-2 mb-2 text-white zoom" style="background:orange;">Gold 750-2000 Points</div>
+    <div class="p-2 mb-2 text-white zoom" style="background:#6a0dad;">Platinum 2000 Points Above</div>
+  </div>
+  <div class="text-center">
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
 
 
 <?php
