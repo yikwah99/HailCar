558c28bde6e09564148b712107734d5fe9899080
CJuan99 <36300446+CJuan99@users.noreply.github.com>
2020-03-28 03:08:39 +0800

    Completed

--- express.php
+++ express.php
@@ -72,7 +72,100 @@
     </div>
 
   </div>
+  <section class="bg-warning">
 
+
+<div class="container ">
+
+  <h2 class="pb-4 text-dark font-weight-bold text-center" >HAIL Express Package Guide</h2>
+  <div class="table-responsive ">
+   <table class="table table-bordered table-hover table-light table-striped shadow w-100">
+    <thead class="thead-dark">
+    <tr>
+     <th colspan="2" class="text-center" style="font-size:22px;">HAIL Express</th>
+    </tr>
+     </thead>
+     <tr>
+      <td> Size </td>
+      <td>43cm x 43cm x 43cm</td>
+     </tr>
+     <tr>
+      <td> Weight</td>
+      <td>Below 10kg</td>
+     </tr>
+
+ </table>
+</div>
+</div>
+</section>
+
+<section class="">
+
+
+<!--<div class="container ">
+
+<h2 class="pb-4 text-dark font-weight-bold text-center" >HAIL Express Rates</h2>
+<div class="table-responsive ">
+ <table class="table table-bordered table-hover table-light table-striped shadow w-100">
+  <thead class="thead-dark">
+  <tr>
+   <th colspan="2" class="text-center" style="font-size:26px;">Fare</th>
+  </tr>
+   </thead>
+   <tr>
+    <td> Base fare</td>
+    <td>RM3.60</td>
+   </tr>
+   <tr>
+    <td> Weight</td>
+    <td>Below 10kg</td>
+   </tr>
+   <tr>
+    <td> Per km rate</td>
+    <td>First 0-6km +RM0.60</td>
+    <td>Next 10-20km +RM1.00</td>
+    <td>Next additional km +RM2.00</td>
+   </tr>
+
+
+</table>
+</div>
+</div>
+</section>-->
+
+<div class="container mb-5" id="ex">
+  <h2 class="pb-4 text-dark font-weight-bold text-center" >HAIL Express Rates</h2>
+	<div class="row">
+<div class="col-md-6 col-lg-6 pb-5 border border-white rounded">
+    <img class=" imgEx" src="img/parcel.jpg" alt="delivery">
+    <div class="overlay oEx ">
+    <button type="button" class=" textEx btn btn-lg btn-warning btn-circle  text-center  " data-toggle="modal" data-target="#login" data-toggle="modal" data-target="#login">Try It Now</button>
+  </div>
+</div>
+		<div class="col-md-6 col-lg-6 ">
+
+			<ul class="timeline">
+				<li>
+					<h3>Base fare</h3>
+				<!--	<a href="#" class="float-right">21 March, 2014</a>-->
+					<p>RM3.60</p>
+				</li>
+				<li>
+					<h3>Per km rate</h3>
+
+					<p>First 0-6km +RM0.60</p>
+          	<p>Next 6-10km +0.70</p>
+            <p>Next 10-20km +RM1.00</p>
+				</li>
+				<li>
+          <h3>Surcharge between 11am to 2pm and 6pm to 9pm</h3>
+          <p>Between RM3-5; depending on supply and demand conditions</p>
+
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
 
 
 
