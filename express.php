29a1d142ffdf4e7587e6bf8633ef408337aa7d5f
CJuan99 <36300446+CJuan99@users.noreply.github.com>
2020-03-28 02:13:30 +0800

    Otw Express

---
+++ express.php
@@ -0,0 +1,111 @@
+<?php
+  $page_title = "Hail Express Services";
+  include "header.php";
+?>
+
+<header class="masthead">
+    <div class="container jumbotron-padding  text-center">
+    <h3 class="display-4 font-weight-bold text-white ">HAIL Express Delivery Services</h3>
+       <p class="paral text-white">
+         An on-demand delivery service that is safe, fast, and reliable
+       </p>
+         <a href="" class="btn btn-lg btn-warning btn-circle my-4 mr-3">Check Out Now</a>
+    </div>
+  </header>
+
+  <div class="container">
+      <h2 class="px-4 py-4 text-dark" >Why Should You Use HAIL Express?</h2>
+    <div class="row">
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+              <div class="our-services-wrapper mb-60">
+                <div class="services-inner">
+                  <div class="our-services-img">
+                  <img src="img/stopwatch.png" width="88px" alt="">
+                  </div>
+                  <div class="our-services-text">
+                    <h4>Fast and Convenient Delivery Service </h4>
+                    <p>Instantly book a door to door delivery for your documents and parcels</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+              <div class="our-services-wrapper mb-60">
+                <div class="services-inner">
+                  <div class="our-services-img">
+                  <img src="img/tick.png" width="88px" alt="">
+                  </div>
+                  <div class="our-services-text">
+                    <h4>Delivered Safely and with Care</h4>
+                    <p>Hand over your parcels to our well-trained drivers, and track the delivery real-time.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+              <div class="our-services-wrapper mb-60">
+                <div class="services-inner">
+                  <div class="our-services-img">
+                  <img src="img/cmoney.png" width="88px" alt="">
+                  </div>
+                  <div class="our-services-text">
+                    <h4>Competitive Pricing</h4>
+                    <p>Competitive upfront fixed fares for all deliveries</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+              <div class="our-services-wrapper mb-60">
+                <div class="services-inner">
+                  <div class="our-services-img">
+                  <img src="img/shield.png" width="88px" alt="">
+                  </div>
+                  <div class="our-services-text">
+                    <h4>Deliver with Peace of Mind </h4>
+                    <p>Every delivery is insured for up to RM 500</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content shadow-lg rounded " >
+        <div class=" text-center py-3 ">
+          <button type="button" class="close pr-2 text-success" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title text-dark">Download The <span class="text-warning" > HAIL</span>  App Now</h4>
+
+        </div>
+        <div class="modal-body" >
+
+          <div class="login px-2 mx-auto mw-100 ">
+                <img class="d-block w-100" src="img/d.png" alt="Second slide">
+                <div class="footer-app">
+                <img src="img/appstore.png" class="app" alt="">
+                  <img src="img/googleplay.png" class="w-50 " alt="">
+                </img>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+</script>
+<?php
+  include "footer.php";
+?>
